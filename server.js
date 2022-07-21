46a0fd8ec3ca76939c00ce83d7ac83958cba7b57
nirmitanallapaneni <69447548+nirmitanallapaneni@users.noreply.github.com>
2022-07-21 14:40:40 -0500

    changes

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-function smail(email2, name)
+function smail(email2, name) {
+    const nodemailer = require('nodemailer');
+    let mailOptions = {
+        from: "nnirmita82@gmail.com",
+        to: email2,
+        subject: "Thanks for your Feedback",
+        text: "Dear " + name + ", Thank you for your feedback! \n\nNirmita N \n https://mysterious-waters-08335.herokuapp.com/"
+    };
 
-{const nodemailer=require('nodemailer');
-let mailOptions = {
-    from: "nnirmita82@gmail.com",
-    to: email2,
-    subject: "Thanks for your Feedback",
-    text: "Dear "+name+", Thank you for your feedback! \n\nNirmita N \n https://feedback-check1.herokuapp.com/"
-};
 
-
-let transporter = nodemailer.createTransport({
-    service:'gmail',
-auth: {
-    user: "nnirmita82@gmail.com",
-    pass: "lhhtdgqjaqrwshix"
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "nnirmita82@gmail.com",
+            pass: "lhhtdgqjaqrwshix"
+        }
+    })
+        .sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error.message);
+            }
+            else {
+                console.log('Email has been sent to user');
+            }
+        });
+    return;
 }
-})
-.sendMail(mailOptions, (error, info) => {
-    if (error) {
- console.log(error.message);
-}
-else{
-console.log('Email has been sent to user');}
-});
-return ;
-}
-module.exports= { smail };
+module.exports = { smail };
